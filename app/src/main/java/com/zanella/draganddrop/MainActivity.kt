6f9a11462d77e85659b4503cfe6b83ec83f20be6
RafaelScotti <rafael.scotti@acad.pucrs.br>
2019-06-16 01:26:34 -0300

    data binding working

--- app/src/main/java/com/zanella/draganddrop/MainActivity.kt
+++ app/src/main/java/com/zanella/draganddrop/MainActivity.kt
@@ -1,12 +1,20 @@
 package com.zanella.draganddrop
 
+import android.databinding.DataBindingUtil
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
+import com.zanella.draganddrop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    var text = "Write here"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        val binding =  DataBindingUtil
+            .setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+        binding.main = MainActivity()
     }
 }
